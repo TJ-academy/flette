170e86e30d66219fe0b4861b412da252f2dea5fc
ssong <9yeosong@naver.com>
2025-08-07 15:06:00 +0900

    깃 연동

---
+++ src/main/java/com/example/flette/FletteApplication.java
@@ -0,0 +1,13 @@
+package com.example.flette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FletteApplication.class, args);
+	}
+
+}
