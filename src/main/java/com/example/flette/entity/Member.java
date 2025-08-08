ae940b54aa8760676ea7e01d45dd25ea7fbc3546
guykim <life4guy@gmail.com>
2025-08-08 11:48:17 +0900

    member entity

---
+++ src/main/java/com/example/flette/entity/Member.java
@@ -0,0 +1,67 @@
+package com.example.flette.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+	@Id
+	private String userid;
+
+	private String passwd;
+	private String username;
+
+	@ColumnDefault("1")
+	private int level;
+
+	private String zipcode;
+	private String address1;
+	private String address2;
+	private String tel;
+	private Date joinedAt;
+
+	/*
+	@OneToMany(mappedBy = "member")
+	List<Cart> cartList = new ArrayList<>();
+
+	@ToString.Exclude
+	@OneToMany(mappedBy = "member")
+	List<Review> reviewList = newArrayList<>();
+
+	@ToString.Exclude
+	@OneToMany(mappedBy = "member")
+	List<Question> questionList = newArrayList<>();
+
+	@ToString.Exclude
+	@OneToMany(mappedBy = "member")
+	List<Answer> answerList = newArrayList<>();
+
+	@ToString.Exclude
+	@OneToMany(mappedBy = "member")
+	List<OrderItem> orderItemList = newArrayList<>();
+
+	@ToString.Exclude
+	@OneToMany(mappedBy = "user")
+	List<Summary> summaryList = newArrayList<>();
+
+	*/
+    public Member(String userid) {
+        this.userid = userid;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        level = level == 0 ? 1 : level;
+    }
+}
--- src/main/java/com/example/flette/entity/User.java
+++
@@ -1,5 +0,0 @@
-package com.example.flette.entity;
-
-public class User {
-
-}
