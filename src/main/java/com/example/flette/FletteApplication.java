0d45de61e5d0387f34abb230eed1c96547bc4599
KangHannarae <hann52156@gmail.com>
2025-08-26 17:35:27 +0900

    ssibal

--- src/main/java/com/example/flette/FletteApplication.java
+++ src/main/java/com/example/flette/FletteApplication.java
@@ -2,12 +2,18 @@ package com.example.flette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FletteApplication {
+public class FletteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FletteApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FletteApplication.class);
+	}
 }
