9cb78d4e47df7ffd51b2922bfbc6e1fe81a9e39f
KangHannarae <hann52156@gmail.com>
2025-09-01 15:54:59 +0900

    dd

--- src/main/java/com/example/flette/config/WebConfig.java
+++ src/main/java/com/example/flette/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-                .allowedOrigins("http://localhost:3000") // 허용할 출처를 명시적으로 지정
+                .allowedOriginPatterns("http://localhost:*", "http://192.168.0.*:*") // 패턴 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 자격 증명 허용
